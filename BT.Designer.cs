7179e2b30282e8fd7559f41ab42289cbfd0e9579
2212374 <2212374@dlu.edu.vn>
2024-08-18 13:43:22 +0700

    Desktop

---
+++ BT.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Homeword_VDHoang_2212374
+{
+	partial class BT
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Text = "BT";
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox grB1;
+		private System.Windows.Forms.Button btn1;
+		private System.Windows.Forms.TextBox txtBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TextBox txtMaSo;
+		private System.Windows.Forms.TextBox txtBox2;
+		private System.Windows.Forms.Button btn2;
+		private System.Windows.Forms.TextBox txtSDT;
+		private System.Windows.Forms.TextBox txtNamSinh;
+		private System.Windows.Forms.TextBox txtDiaChi;
+		private System.Windows.Forms.TextBox txtHoTen;
+		private System.Windows.Forms.Label lblHoTen;
+		private System.Windows.Forms.Label lblMaSo;
+		private System.Windows.Forms.Label lblDiaChi;
+		private System.Windows.Forms.Label lblNamSinh;
+		private System.Windows.Forms.Label lblSDT;
+		private System.Windows.Forms.ImageList imageList;
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolbarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem statusBarToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem1;
+		private System.Windows.Forms.ToolStripMenuItem strToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem actuzisToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem nextToolStripMenuItem;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.LinkLabel linkLabel2;
+		private System.Windows.Forms.Panel pn35;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Panel pn42;
+		private System.Windows.Forms.TextBox txtDescription;
+		private System.Windows.Forms.TextBox txtPassword;
+		private System.Windows.Forms.TextBox txtUser;
+		private System.Windows.Forms.Label lblDescription;
+		private System.Windows.Forms.Label lblPassword;
+		private System.Windows.Forms.Label lblUser;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.Button btnOk;
+		private System.Windows.Forms.Panel pn48;
+		private System.Windows.Forms.ComboBox cboCNN;
+		private System.Windows.Forms.Label lblCNN;
+		private System.Windows.Forms.ListBox lbDanhsach;
+		private System.Windows.Forms.Button btnAdd;
+		private System.Windows.Forms.Panel pn52;
+		private System.Windows.Forms.ListBox lbDanhsachChon;
+		private System.Windows.Forms.Button btnRemore;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel pn56;
+		private System.Windows.Forms.RadioButton rdGirl;
+		private System.Windows.Forms.RadioButton rdMen;
+		private System.Windows.Forms.GroupBox grbGT;
+		private System.Windows.Forms.Panel pn57;
+		private System.Windows.Forms.CheckBox ckbMauNen;
+		private System.Windows.Forms.CheckBox ckbMauChu;
+		private System.Windows.Forms.Button btnButtonFlat;
+		private System.Windows.Forms.Button btnButton;
+		private System.Windows.Forms.RadioButton rdPopup;
+		private System.Windows.Forms.RadioButton rdFlat;
+		private System.Windows.Forms.Panel pn60;
+		private System.Windows.Forms.CheckedListBox clbMonHoc;
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Homeword_VDHoang_2212374;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Homework_VDHoang_2212374
+{
+	internal class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new BT());
+		}
+	}
+}
